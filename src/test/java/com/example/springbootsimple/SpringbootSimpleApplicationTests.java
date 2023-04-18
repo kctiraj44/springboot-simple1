f4310b5f4db8edf325044bcaf5b3c282c68082ca
kctiraj44 <kctiraj44@gmail.com>
2023-04-18 14:54:46 -0500

    First commit

---
+++ src/test/java/com/example/springbootsimple/SpringbootSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
